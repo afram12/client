6a2ce4eb3ad501a210b08471ed080e65e42c5f20
afram12 <tanishkshrivastav23@gmail.com>
2025-08-19 14:28:57 +0530

    new

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react'
 import {
-    Link,
-    useNavigate
-}from "react-router-dom"
+  Link,
+  useNavigate
+} from "react-router-dom"
 import API from "../services/api";
 
 function Login() {
@@ -15,50 +15,50 @@ function Login() {
 
 
 
-        try {
-            const res= await API.post("/login", {email, password});
+    try {
+      const res = await API.post("/login", { email, password });
 
-            if(res.data.message == "Login successful"){
-                //user data to localhost me store karein 
-                localStorage.setItem ("user",JSON.stringify(res.data));
+      if (res.data.message == "Login successful") {
+        //user data to localhost me store karein 
+        localStorage.setItem("user", JSON.stringify(res.data));
 
-                alert("Login successful !");
-                navigate("/");
-            } else {
-                alert (res.data.message || "login failed");
-            }
-        }catch (error){
-                console.error(error);
-                alert("something went wrong!");
-            }
-        }
+        alert("Login successful !");
+        navigate("/");
+      } else {
+        alert(res.data.message || "login failed");
+      }
+    } catch (error) {
+      console.error(error);
+      alert("something went wrong!");
+    }
+  }
 
-        return (
-    <div className="container mt-5" style={{ maxWidth: "500px"}}>
+  return (
+    <div className="container mt-5" style={{ maxWidth: "500px" }}>
       <h2 className="text-center mb-4">Login</h2>
       <form onSubmit={handleSubmit}>
         <div className="mb-3">
           <label>Email address</label>
-          <input 
+          <input
             type="email"
             className="form-control"
             placeholder="Enter email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
-            required 
+            required
           />
         </div>
 
         <div className="mb-3">
           <label >Password</label>
-          <input 
+          <input
             type="password"
             className="form-control"
             placeholder="Enter password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
-            required 
-            />
+            required
+          />
         </div>
         <button type="submit" className="btn btn-primary w-100">
           Login
@@ -67,8 +67,8 @@ function Login() {
     </div>
   );
 
-    }
-  
+}
+
 
 
 export default Login
